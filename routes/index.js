eaad938dcb69981c21204f866bc910a0aa0dccd3
KhairiTab <taboubikhairi68@gmail.com>
2025-04-13 21:22:28 +0100

    update file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // routes/index.js
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
@@ -10,4 +11,6 @@ router.get('/stats', AppController.getStats);
 
 // User routes
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
 export default router;
