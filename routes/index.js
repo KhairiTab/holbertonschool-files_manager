6b53ea203d92aec97d36d718c3638fc0d6b67299
KhairiTab <taboubikhairi68@gmail.com>
2025-04-13 21:30:40 +0100

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -12,5 +14,9 @@ router.get('/stats', AppController.getStats);
 // User routes
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
